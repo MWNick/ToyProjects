0a9bcae0ee8332b39009b5354cb92c9201a12c06
ktnetadmin <hmw9309@gmail.com>
2022-12-13 11:23:50 +0900

    Init Springboot Project

---
+++ toypayments/toypament/src/main/java/toy/payment/toypament/ToypamentApplication.java
@@ -0,0 +1,13 @@
+package toy.payment.toypament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToypamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToypamentApplication.class, args);
+	}
+
+}
